3e9f524547de3cbb7610fefffe01382188c2740a
Lilian Xiao <lilianxia456@gmail.com>
2025-09-28 16:23:50 -0400

    Update to tricolor toon shader + make new materials

--- Assets/Shaders/Includes/LightingHelp.hlsl
+++ Assets/Shaders/Includes/LightingHelp.hlsl
@@ -20,11 +20,15 @@ void GetMainLight_float(float3 WorldPos, out float3 Color, out float3 Direction,
 #endif
 }
 
-void ChooseColor_float(float3 Highlight, float3 Shadow, float Diffuse, float Threshold, out float3 OUT)
+void ChooseColor_float(float3 Highlight, float3 Midtone, float3 Shadow, float Diffuse, float Threshold, out float3 OUT)
 {
-    if (Diffuse < Threshold)
+    if (Diffuse < (Threshold / 3))
     {
         OUT = Shadow;
+    }
+    else if (Diffuse >= (Threshold / 3) && Diffuse < (2 * Threshold / 3))
+    {
+        OUT = Midtone;
     }
     else
     {
